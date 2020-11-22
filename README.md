42d534afca5ce851da146ef9c7c1c9a1ea1f9dee
David Needles <dpn28@cornell.edu>
2020-11-21 21:26:34 -0500

    Added code and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# clmu - Common Lisp Manga Updater
+A simple lisp program that scrapes [mangaupdates](www.mangaupdates.com) to
+print out the latest released chapter for a given list of manga. 
+
+# Usage
+Add manga using the template provided in the script. Run `(main)` to print out
+the information.
---
+++ clmu.lisp
@@ -0,0 +1,91 @@
+(asdf:load-system :dexador)
+(asdf:load-system :lquery)
+
+;; Add manga here
+(defvar *manga*
+  '(
+;;    (:title "Manga 1"
+;;     :url "https://www.mangaupdates.com/releases.html?search=[xxxx]&stype=series")
+;;    (:title "Manga 2"
+;;     :url "https://www.mangaupdates.com/releases.html?search=[xxxx]&stype=series")
+    ))
+
+(defun spaces (n acc)
+  (if (= n 0) acc (spaces (- n 1) (concatenate 'string " " acc))))
+
+(defun release (title date chapter group)
+  (list :title title
+	:date date
+	:chapter chapter
+	:group group))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-manga ()
+  (manga
+   (prompt-read "Title:")
+   (prompt-read "Url:")))
+
+(defun load-url (url)
+  (let ((request (dex:get url)))
+    (lquery:$ (initialize request))))
+
+(defun parse-date (page)
+  (let ((dates (lquery:$ page "#main_content .col-2" (text))))
+    (elt dates 1)))
+
+(defun parse-chapter (page)
+  (let ((chapt (lquery:$ page "#main_content .col-1" (text))))
+    (elt chapt 3)))
+
+(defun parse-group (page)
+  (let ((group (lquery:$ page "#main_content .col-4" (text))))
+    (elt group 3)))
+
+(defun parse-release (m)
+  (let ((page (load-url (getf m :url))))
+    (release
+     (getf m :title)
+     (parse-date page)
+     (parse-chapter page)
+     (parse-group page))))
+
+(defun get-len (rl id)
+  (apply #'max (mapcar (lambda (x) (length (getf x id))) rl)))
+
+(defun format-id (r n id)
+  (let ((str (getf r id)))
+    (if (eq id :title)
+	    (concatenate 'string str (spaces (- n (length str)) ""))
+    	    (concatenate 'string (spaces (- n (length str)) "") str))))
+
+(defun print-release (r tl cl)
+  (format t "~a | ~a | ~a | ~a~%"
+	  (getf r :date)
+	  (format-id r tl :title)
+	  (format-id r cl :chapter)
+	  (getf r :group)))
+
+(defun compare-date (d1 d2)
+  (let ((y1 (parse-integer (subseq d1 6 8)))
+	(y2 (parse-integer (subseq d2 6 8)))
+	(m1 (parse-integer (subseq d1 0 2)))
+	(m2 (parse-integer (subseq d2 0 2)))
+	(d1 (parse-integer (subseq d1 3 5)))
+	(d2 (parse-integer (subseq d2 3 5))))    
+    (if (not (equal y1 y2)) (> y1 y2)
+	(if (not (equal m1 m2)) (> m1 m2) (> d1 d2)))))
+
+(defun get-releases ()
+  (sort
+   (mapcar #'parse-release *manga*)
+   (lambda (r1 r2) (compare-date (getf r1 :date)
+				 (getf r2 :date)))))
+(defun main ()
+  (let ((rels (get-releases)))
+    (mapcar (lambda (r)
+	      (print-release r (get-len rels :title) (get-len rels :chapter)))
+	    rels)))
